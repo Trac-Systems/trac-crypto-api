f27c7b89278d3f3d1c886f3494eecce3316ee052
leonardostsouza <leonardo.souza@trac.network>
2025-08-20 12:27:38 +0200

    Implement API modules

---
+++ index.js
@@ -0,0 +1,16 @@
+import address from './modules/address.js';
+import hash from './modules/hash.js';
+import mnemonic from './modules/mnemonic.js';
+import nonce from './modules/nonce.js';
+import signature from './modules/signature.js';
+
+const sign = signature.sign;
+
+export default {
+    address,
+    hash,
+    mnemonic,
+    nonce,
+    signature,
+    sign
+};
