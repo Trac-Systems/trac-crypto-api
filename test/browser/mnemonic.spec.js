78a687e954bda3561db750ab5246aa041f4ae8ba
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2025-08-25 11:25:36 -0300

    feat(test): Add satize browser tests for mnemonic module

--- test/browser/mnemonic.spec.js
+++ test/browser/mnemonic.spec.js
@@ -13,3 +13,35 @@ test("should generate a mnemonic in the browser environment", async ({
   expect(typeof mnemonicInBrowser).toBe("string");
   expect(mnemonicInBrowser.split(" ").length).toBe(24);
 });
+
+test("should sanitize a mnemonic phrase in the browser using direct evaluation", async ({
+  page,
+}) => {
+  // Expose the sanitize function to the browser's window object
+  await page.exposeFunction("sanitizeInBrowser", allModules.mnemonic.sanitize);
+
+  const expectedPhrase = allModules.mnemonic.generate();
+  const sabotagedPhrase = expectedPhrase.replaceAll(" ", "   ").toUpperCase();
+
+  const sanitizedMnemonic = await page.evaluate((mnemonic) => {
+    // This code runs in the browser context
+    return window.sanitizeInBrowser(mnemonic);
+  }, sabotagedPhrase);
+
+  expect(sanitizedMnemonic).toBe(expectedPhrase);
+});
+
+test("should throw an error while sanitizing a wrong mnemonic", async ({
+  page,
+}) => {
+  // Expose the sanitize function to the browser's window object
+  await page.exposeFunction("sanitizeInBrowser", allModules.mnemonic.sanitize);
+  const sabotagedPhrase = "wrong mnemonic phrase";
+
+  // Expect an error to be thrown when sanitizing an invalid mnemonic
+  await expect(
+    page.evaluate((mnemonic) => {
+      return window.sanitizeInBrowser(mnemonic);
+    }, sabotagedPhrase)
+  ).rejects.toThrow("Invalid mnemonic phrase");
+});
