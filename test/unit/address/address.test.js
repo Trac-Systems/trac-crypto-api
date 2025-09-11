c65dc63f178abb96645620e366b71095741d70d9
leonardostsouza <leonardo.souza@trac.network>
2025-09-11 12:03:09 +0200

    Implement key derivation

--- test/unit/address/address.test.js
+++ test/unit/address/address.test.js
@@ -1,70 +1,12 @@
-const test = require("brittle");
-const api = require("../../../index.js");
-const { TRAC_PUB_KEY_SIZE, TRAC_PRIV_KEY_SIZE } = require("../../../constants.js");
-const b4a = require("b4a");
+const test = require('brittle');
 
-const hrp = "trac";
+function runTests() {
+  test.pause();
 
-test("address.generate: should generate a valid address and keypair with no mnemonic input", async (t) => {
-  const result = await api.address.generate(hrp);
-  t.is(typeof result.address, "string");
-  t.ok(b4a.isBuffer(result.publicKey));
-  t.is(result.publicKey.length, TRAC_PUB_KEY_SIZE);
-  t.ok(b4a.isBuffer(result.secretKey));
-  t.is(result.secretKey.length, TRAC_PRIV_KEY_SIZE);
-  t.is(typeof result.mnemonic, "string");
-});
+  require('./encoding.test.js');
+  require('./generate.test.js');
 
-test("address.generate: should generate the same keypair for the same mnemonic", async (t) => {
-  const mnemonic = api.mnemonic.generate();
-  const result1 = await api.address.generate(hrp, mnemonic);
-  const result2 = await api.address.generate(hrp, mnemonic);
-  t.ok(b4a.equals(result1.publicKey, result2.publicKey));
-  t.ok(b4a.equals(result1.secretKey, result2.secretKey));
-  t.is(result1.mnemonic, result2.mnemonic);
-  t.is(result1.address, result2.address);
-});
+  test.resume();
+}
 
-test("address.encode/decode: should encode and decode public key correctly", async (t) => {
-  const { publicKey } = await api.address.generate(hrp);
-  const encoded = api.address.encode(hrp, publicKey);
-  const decoded = api.address.decode(encoded);
-  t.ok(decoded.equals(publicKey));
-});
-
-test("address.encode: should throw on invalid public key", (t) => {
-  try {
-    api.address.encode(hrp, Buffer.alloc(10));
-    t.fail();
-  } catch {
-    t.pass();
-  }
-});
-
-test("address.decode: should throw on invalid address", (t) => {
-  try {
-    api.address.decode("invalidaddress");
-    t.fail();
-  } catch {
-    t.pass();
-  }
-});
-
-test("address.generate: should return an error for invalid mnemonic", async (t) => {
-  try {
-    const mnemonic = "invalid mnemonic";
-    const result = await api.address.generate(hrp, mnemonic);
-    t.fail();
-  } catch {
-    t.pass(); // TODO: In the future, assert the error message
-  }
-});
-
-test("address.generate: should return an error for missing hrp", async (t) => {
-  try {
-    const result = await api.address.generate(null);
-    t.fail();
-  } catch (error) {
-    t.pass(); // TODO: In the future, assert the error message
-  }
-});
+runTests();
