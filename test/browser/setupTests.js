2cc572bcf0a066b10e8df67a3a0f27f2c8c095b2
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2025-09-13 11:28:45 -0300

    utils tests with jest

--- test/browser/setupTests.js
+++ test/browser/setupTests.js
@@ -1,2 +1,4 @@
 const TracCryptoApi = require("trac-crypto-api")
+const b4a = require("b4a")
 window.TracCryptoApi = TracCryptoApi
+window.b4a = b4a
---
+++ test/browser/utils/utils.test.js
@@ -0,0 +1,45 @@
+// utils.test.js
+const { utils } = require("trac-crypto-api")
+const b4a = require("b4a")
+
+test("utils is on window", () => {
+  expect(window.TracCryptoApi.utils).toBe(utils)
+})
+
+test("b4a is on window", () => {
+  expect(window.b4a).toBe(b4a)
+})
+
+
+test("should correctly zero out a buffer in the browser", async () => {
+  // Access the exposed API from the window object.
+  const { memzero } = window.TracCryptoApi.utils
+  const b4a = window.b4a
+
+  // Create a buffer and fill it with non-zero values.
+  const buffer = b4a.alloc(16)
+  buffer.fill(0xff)
+
+  // Call the memzero function.
+  memzero(buffer)
+
+  // Return true if all bytes are zero, false otherwise.
+  const isZeroed = buffer.every((byte) => byte === 0)
+
+  expect(isZeroed).toBe(true)
+})
+
+test("should not change a non-buffer object in the browser", async () => {
+
+  const { memzero } = window.TracCryptoApi.utils
+
+  const nonBuffer = { a: 1, b: "test" }
+  const originalValue = JSON.stringify(nonBuffer)
+
+  memzero(nonBuffer)
+
+  const isUnchanged = JSON.stringify(nonBuffer) === originalValue
+
+
+  expect(isUnchanged).toBe(true)
+})
