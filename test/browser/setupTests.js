1b4cd5bb603b6c6324a6810e81b86c31425bcd68
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2025-09-13 11:48:51 -0300

    signature tests with jest

--- test/browser/setupTests.js
+++ test/browser/setupTests.js
@@ -1,4 +1,6 @@
 const TracCryptoApi = require("trac-crypto-api")
 const b4a = require("b4a")
+const sodium = require("sodium-universal")
 window.TracCryptoApi = TracCryptoApi
 window.b4a = b4a
+window.sodium = sodium
