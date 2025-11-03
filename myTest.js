d38d9d29e0158310a65c42c2d54e6806a0194fc8
leonardostsouza <leonardo.souza@trac.network>
2025-11-03 17:16:09 +0100

    More functions in myTest.js

--- myTest.js
+++ myTest.js
@@ -20,8 +20,6 @@ const to = "trac1ah5fzvpw7ht0audcm045d343khxsh59njxnz7c6gc7af5cm78hxqtp48jh";
 // const amountDec = 1_000_000_000_000_000_000_000; // 1000 $TNK
 const amountDec = 1_000_000_000_000_000_000; // 1 $TNK
 const amount = numToHex(amountDec);
-console.log("Amount as hex string:", amount);
-console.log("Amount as number:", hextoNum(amount));
 
 // operation parameters
 const originBootstrap = "5adb970a73e20e8e2e896cd0c30cf025a0b32ec6fe026b98c6714115239607ac";
@@ -59,4 +57,38 @@ async function testFunction() {
     console.log("Decoded Payload String: ", decodedStr);
     console.log("Decoded Payload Object:", decodedObj);
 }
-testFunction();
+// testFunction();
+
+function testBuildTransaction() {
+    let from = "trac1w26lfcrlwhnndne2g8wuz0geufhygzttnxxnvnywj492v0amc9ds6jax3r";
+    let to = "trac1nh84vgtdt3phmewcjl5g2msd5zal5dz62qxvxctrt3ekvymsscwql7zjcz";
+    let amount = "0DE0B6B3A763FFFF"; // 1 TNK
+    let validity = "cf639bbf44193db970809a6f3b891f681c13ba54da611d4219629f3de8f2ac05";
+    let networkId = api.MAINNET_ID;
+    let secretKey = "fe4e4e064e7bec80d33cb61a65f09da07631094d80c4aebd83d48ad163f7ab8172b5f4e07f75e736cf2a41ddc13d19e26e44096b998d364c8e954aa63fbbc15b";
+    api.transaction.preBuild(from, to, amount, validity, networkId)
+    .then(preTx => {
+        const tx = api.transaction.build(preTx, b4a.from(secretKey, "hex"));
+        const payload = {
+            payload: tx
+        }
+        console.log(JSON.stringify(payload));
+    });
+}
+// testBuildTransaction();
+
+async function createAddress() {
+    const keyPair = await api.address.generate("trac");
+    const address = keyPair.address;
+    console.log("Generated Address:", address);
+    console.log("Private Key:", b4a.toString(keyPair.secretKey, "hex"));
+}
+// createAddress();
+
+async function isAddressValid(address) {
+    console.log("Testing address: ", address);
+    console.log("Is valid address format?", api.address.isValid(address));
+    console.log("decoded Address Buffer (pub key):", api.address.decode(address).toString('hex'));
+}
+
+isAddressValid("trac19wyqu9g80pn95d8cwkzn3e3qk09rj7tsf4vv4sh8mzjh8dmwcwkqp9rql4");
