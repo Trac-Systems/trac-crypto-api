ec64df7ecf68ef69abab0f0e3b3702b76a878840
leonardotc <ltc270486@hotmail.com>
2026-04-08 15:40:03 +0200

    chore: bump version

--- index.rn.js
+++ index.rn.js
@@ -2,30 +2,33 @@ const address = require('./modules/address.js');
 const hash = require('./modules/hash.js');
 const mnemonic = require('./modules/mnemonic.js');
 const nonce = require('./modules/nonce.js');
-const operation = require('./modules/operation.js');
 const signature = require('./modules/signature.js');
-const transaction = require('./modules/transaction.js');
 const utils = require('./modules/utils.js');
+const transaction = require('./modules/transaction.js');
+const operation = require('./modules/operation.js');
 
 const b4a = require('b4a');
 const sodium = require('sodium-universal');
 
 if (typeof location === 'undefined') {
-  globalThis.location = { href: '' };
+    globalThis.location = { href: '' };
 }
 if (typeof document === 'undefined') {
-  globalThis.document = { currentScript: { src: '' } };
+    globalThis.document = { currentScript: { src: '' } };
 }
 
+const sign = signature.sign;
+
 module.exports = {
-  address,
-  hash,
-  mnemonic,
-  nonce,
-  operation,
-  signature,
-  transaction,
-  utils,
-  b4a,
-  sodium
+    address,
+    hash,
+    mnemonic,
+    nonce,
+    signature,
+    utils,
+    transaction,
+    operation,
+    sign,
+    b4a,
+    sodium
 };
